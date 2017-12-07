702a7b62edda348ab5d0f4165b6b2cc5cda3d9b2
Oli Griffiths <oligriffiths@users.noreply.github.com>
2017-12-07 12:11:24 -0500

    Update SomeTest.php

--- demo/SomeTest.php
+++ demo/SomeTest.php
@@ -7,6 +7,18 @@ class SomeTest extends PHPUnit_Framework_TestCase
     use \OliGriffiths\GUnit\TestTrait;
 
     /**
+     * @param Result $result The result of executing testRequest201
+     * @depends testRequest201
+     */
+    public function testSomething(Result $result)
+    {
+        $result = $this->makeRequest('GET', '/201');
+        $this->assertStatusCode(201, $result);
+        $this->assertHeaderExists('Content-Type', $result);
+        $this->assertHeaderEquals('Content-Type', 'text/plain; charset=utf-8', $result);
+    }
+
+    /**
      * The result of this test will be shared between any other test that depends on it, and not re-run the test
      *
      * @return \OliGriffiths\GUnit\Guzzle\Result
@@ -17,17 +29,9 @@ class SomeTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @param Result $result The result of executing testRequest201
-     * @depends testRequest201
-     */
-    public function testSomething(Result $result)
-    {
-        $this->assertStatusCode(201, $result);
-        $this->assertHeaderExists('Content-Type', $result);
-        $this->assertHeaderEquals('Content-Type', 'text/plain; charset=utf-8', $result);
-    }
-
-    /**
+     * This test receives the $result from testRequest201() as the first argument, allowing multiple tests to
+     * utilize the same result without re-requesting it.
+     *
      * @param Result $result The result of executing testRequest201
      * @depends testRequest201
      */
@@ -36,6 +40,11 @@ class SomeTest extends PHPUnit_Framework_TestCase
         $this->assertBodyEquals('201 Created', $result);
     }
 
+    /**
+     * This test shows how to test for body keys in the decoded JSON response. This doesn't actually make an HTTP
+     * request as httpstat.us doesn't return a key/value JSON response unfortunately, so we're mocking one to show
+     * how the body assert key methods work
+     */
     public function testJsonResponse()
     {
         $request = new \GuzzleHttp\Psr7\Request(
